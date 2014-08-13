7b0e498df4a4338afdb4b7e4f556450cf88ae9d7
halosprtn117 <halosprtn117@gmail.com>
2014-08-13 09:03:06 -0400

    Updated keyFinder.py keyFinder.py now starts by compiling a list of singular hex sets so that there are no duplicates.  This eliminates the posibility of getting false positives when there are many entries which are spaces.

--- OneTimePad/keyFinder.py
+++ OneTimePad/keyFinder.py
@@ -16,17 +16,24 @@ def strxor(a, b):     # xor two strings of different lengths
    
 def search(i,j,MSGS):
     greatestcount = 0
-    for m in MSGS:
+    
+    indexMSGS=[]
+    
+    for x in MSGS:
+        if indexMSGS.count(x[i:j]) == 0:
+            indexMSGS.append(x[i:j])    
+    
+    for m in indexMSGS:
         count=0
         
-        for n in MSGS:
-            temp=strxor(m[i:j].decode('hex'),n[i:j].decode('hex'))
+        for n in indexMSGS:
+            temp=strxor(m.decode('hex'),n.decode('hex'))
             if ((temp>= "A" and temp<= "Z") or (temp>= "a" and temp<= "z")):
                 count+=1
-                print "count: " + str(count) + "    Message M: " + str(MSGS.index(m)) + "    " + m[i:j] + "    Message N: " + str(MSGS.index(n)) + "    " + n[i:j] + "    Xor: " + temp.encode('hex')
+                #print "count: " + str(count) + "    Message M: " + str(MSGS.index(m)) + "    " + m + "    Message N: " + str(MSGS.index(n)) + "    " + n + "    Xor: " + temp.encode('hex')
             if count > greatestcount:
                 greatestcount = count
-                final = m[i:j].decode('hex')
+                final = m.decode('hex')
     if greatestcount > 0:
         return strxor(final,"20".decode('hex')).encode('hex')
     return "00"
@@ -50,7 +57,8 @@ def main():
         
     print findKey(MSGS)
     K.write(findKey(MSGS) + "\n")
-    print search(28,30,MSGS)
+    #print search(28,30,MSGS)
+    #print search(0,2,MSGS)
 
 
 if __name__ == "__main__":
